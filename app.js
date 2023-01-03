2c762912ab9c6c92b0499a390451a5c73707589b
edson ibarra <edsonibarra97@gmail.com>
2023-01-02 20:22:35 -0600

    Working with Variables

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const learnJs = () => {
-    console.log("Hello World");
-};
+let age = 10;
+let personName = 'John Doe';
+const country = 'Hong Kong'; 
 
-learnJs()
+console.log('---- Customer Information ----')
+console.log(`Age: ${age}`);
+console.log(`Person Name: ${personName}`);
+console.log(`From: ${country}`);
