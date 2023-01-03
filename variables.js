a3d70007d276cafc18e44d11b98cdb44d4b17fea
edson ibarra <edsonibarra97@gmail.com>
2023-01-02 20:34:37 -0600

    variables scope example

--- variables.js
+++ variables.js
@@ -6,3 +6,19 @@ console.log('---- Customer Information ----')
 console.log(`Age: ${age}`);
 console.log(`Person Name: ${personName}`);
 console.log(`From: ${country}`);
+
+
+let greeting = 'hola mundo';
+
+console.log(`before funciton ${greeting}`);
+
+const sayHello = () => {
+    let greeting = 'hello world';
+    console.log(`inside funciton ${greeting}`);
+};
+
+console.log('executing function');
+
+sayHello();
+
+console.log(`after funciton ${greeting}`);
