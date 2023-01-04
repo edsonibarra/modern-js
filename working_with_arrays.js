e5e1ec433cc61a52698262b0f4cb5597ff85eb49
edson ibarra <edsonibarra97@gmail.com>
2023-01-03 20:53:47 -0600

    working with arrays

---
+++ working_with_arrays.js
@@ -0,0 +1,22 @@
+let programmingLanguages = [
+    'python',
+    'javascript',
+    'java',
+    'go',
+    'c#',
+    'rust'
+];
+
+let frameworks = [
+    'django',
+    'react',
+    'spring',
+    'spring boot',
+    '.net'
+];
+
+console.log({programmingLanguages, frameworks});
+
+let cplusplus = 'c++';
+programmingLanguages.push(cplusplus);
+console.log({newProgrammingLanguages: programmingLanguages});
