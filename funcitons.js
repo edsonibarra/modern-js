6d9e9235d75c102a611f70b5ea4fc7598ba3e3ef
edson ibarra <edsonibarra97@gmail.com>
2023-01-03 21:24:38 -0600

    functions 101

---
+++ funcitons.js
@@ -0,0 +1,19 @@
+
+
+function sayHello(){
+    console.log('Hello');
+};
+
+sayHello();
+
+// Anonymous funcitons
+
+const anotherFunction = function(){
+    console.log('Hello');
+};
+
+anotherFunction();
+
+
+
+
