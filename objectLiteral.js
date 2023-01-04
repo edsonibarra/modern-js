de35b19e56dca0e72d839e848e4c5e4e9e7f7ec7
edson ibarra <edsonibarra97@gmail.com>
2023-01-03 20:48:47 -0600

    literal objects

--- objectLiteral.js
+++ objectLiteral.js
@@ -3,7 +3,16 @@ let character = {
     alias: 'IronMan',
     isAlive: true,
     age: 40,
-    superpowers: ['fly', 'strength', 'speed']
+    superpowers: ['fly', 'strength', 'speed'],
+    address: {
+        zip: 999,
+        country: 'USA',
+    },
 }
 
 console.log(character)
+console.log(character.name);
+console.log(character.alias);
+console.log(character.isAlive);
+console.log(character.superpowers[0]);
+console.log(character.address.zip);
